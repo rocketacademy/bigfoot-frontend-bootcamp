2aa11b6dfcde1250cdc75f296aad208458dceb6b
neokaiyuan <kai@rocketacademy.co>
2022-06-02 16:58:20 +0800

    Convert SightingPreview to functional component to be consistent

--- src/components/SightingPreview.js
+++ src/components/SightingPreview.js
@@ -1,18 +1,16 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
 
-class SightingPreview extends React.Component {
-  render() {
-    return (
-      <Card bg="dark">
-        <Card.Body>
-          <Card.Title>
-            {`${this.props.data.YEAR} ${this.props.data.SEASON} ${this.props.data.MONTH}`}
-          </Card.Title>
-        </Card.Body>
-      </Card>
-    );
-  }
-}
+const SightingPreview = (props) => {
+  return (
+    <Card bg="dark">
+      <Card.Body>
+        <Card.Title>
+          {`${props.data.YEAR} ${props.data.SEASON} ${props.data.MONTH}`}
+        </Card.Title>
+      </Card.Body>
+    </Card>
+  );
+};
 
 export default SightingPreview;
