b116dbe0c520737f538d7b418040380f6cca0621
neokaiyuan <kai@rocketacademy.co>
2022-06-02 17:01:27 +0800

    Remove unused import

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Link, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import "./index.css";
 import App from "./components/App";
