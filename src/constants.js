073a5c3dd6e035657b56d0a57b9a9cdc529bd435
neokaiyuan <kai@rocketacademy.co>
2022-06-07 00:02:31 +0800

    Deployed to netlify with production backend url link from env var

--- src/constants.js
+++ src/constants.js
@@ -3,4 +3,7 @@
  * We will need to dynamically set BACKEND_URL based on the value
  * of process.env.NODE_ENV when we wish to deploy to production.
  */
-export const BACKEND_URL = "http://localhost:3000";
+export const BACKEND_URL =
+  process.env.NODE_ENV === "production"
+    ? "https://powerful-caverns-02560.herokuapp.com"
+    : "http://localhost:3000";
