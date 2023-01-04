fe363ecf8e6d95e7f13bfaffbf5bbc4ba879fb58
sharlearn <liewsharlen@gmail.com>
2023-01-05 00:56:53 +0800

    Completed Base: Retrieve Sightings

--- src/App.js
+++ src/App.js
@@ -1,20 +1,44 @@
-import React from "react";
-import logo from "./logo.png";
+import React, { useEffect } from "react";
+import logo from "./bigfoot.jpg";
 import "./App.css";
+import axios from "axios";
+import { useState } from "react";
+import { Link, Outlet } from "react-router-dom";
+import { BACKEND_URL } from "./constants";
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-        </header>
+export default function App() {
+  const [allSightings, setAllSightings] = useState([]);
+
+  useEffect(() => {
+    axios.get(`${BACKEND_URL}/sightings`).then((response) => {
+      console.log(response.data);
+      setAllSightings(response.data);
+    });
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>BIGFOOT REPORTS</h1>
+      </header>
+      <div className="App-body">
+        <div>
+          <h1>Reported Sightings</h1>
+          <ul className="sightings-list">
+            {allSightings.map((sighting, index) => (
+              <li key={index}>
+                <Link to={`/sighting/${index}`}>
+                  {sighting.YEAR}, {sighting.SEASON}, {sighting.STATE}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="Sighting-info">
+          <Outlet />
+        </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,27 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import SightingInfo from "./Sighting";
+import ErrorPage from "./error-page";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "sighting/:sightingID",
+        element: <SightingInfo />,
+      },
+    ],
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
