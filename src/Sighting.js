6e17275bee5d16d4e783e304bdf79aa41677ec9c
yomansg <br.networkers@gmail.com>
2023-01-06 22:14:06 +0800

    Retrieve sighting data in frontend

---
+++ src/Sighting.js
@@ -0,0 +1,54 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import Card from "react-bootstrap/Card";
+
+import { BACKEND_URL } from "./constants.js";
+
+const Sighting = () => {
+  const [sightingIndex, setSightingIndex] = useState();
+  const [sighting, setSighting] = useState();
+
+  useEffect(() => {
+    if (sightingIndex) {
+      axios
+        .get(`${BACKEND_URL}/sightings/${sightingIndex}`)
+        .then((response) => {
+          setSighting(response.data);
+        });
+    }
+    // Only run this effect on change to sightingIndex
+  }, [sightingIndex]);
+
+  // Update sighting index in state if needed to trigger data retrieval
+  const params = useParams();
+  if (sightingIndex !== params.sightingIndex) {
+    setSightingIndex(params.sightingIndex);
+  }
+
+  const sightingDetails = [];
+  if (sighting) {
+    for (const key in sighting) {
+      sightingDetails.push(
+        <Card.Text key={key}>{`${key}: ${sighting[key]}`}</Card.Text>
+      );
+    }
+  }
+
+  return (
+    <div>
+      <Link to="/">Home</Link>
+      <Card bg="dark">
+        <Card.Body>
+          <Card.Title>
+            {sighting &&
+              `${sighting.YEAR} ${sighting.SEASON} ${sighting.MONTH}`}
+          </Card.Title>
+          {sightingDetails}
+        </Card.Body>
+      </Card>
+    </div>
+  );
+};
+
+export default Sighting;
