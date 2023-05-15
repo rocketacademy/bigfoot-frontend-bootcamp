aea4d95963b5cce6524a1fdc851f7a0f9ed6b716
prettykaur <pkaur.dev@gmail.com>
2023-05-15 16:46:05 +0800

    Add router functionality

--- src/index.js
+++ src/index.js
@@ -2,6 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SightingsList from "./Components/SightingsList";
+import Sighting from "./Components/Sighting";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/sightings" element={<SightingsList />} />
+      <Route path="/sightings/:sightingIndex" element={<Sighting />} />
+      <Route path="*" element={"Oops! Something went wrong."} />
+      {/* </Route> */}
+    </Routes>
+  </BrowserRouter>
+);
