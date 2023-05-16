3dd689232b4fc64d98f4db54fa1c0588a96c82f0
prettykaur <pkaur.dev@gmail.com>
2023-05-16 21:25:08 +0800

    Add filter functionality

---
+++ src/Components/Filter.js
@@ -0,0 +1,44 @@
+import React from "react";
+
+function Filter({ filters, onFilterChange, onSubmit }) {
+  return (
+    <div>
+      <h2 className="filter-header">Filter Sightings</h2>
+
+      <form onSubmit={onSubmit} className="search-form">
+        <label htmlFor="year">Year:</label>
+        <input
+          type="text"
+          name="year"
+          id="year"
+          value={filters.year}
+          onChange={onFilterChange}
+          className="search-input"
+        />
+        <label htmlFor="season">Season:</label>
+        <input
+          type="text"
+          name="season"
+          id="season"
+          value={filters.season}
+          onChange={onFilterChange}
+          className="search-input"
+        />
+        <label htmlFor="month">Month:</label>
+        <input
+          type="text"
+          name="month"
+          id="month"
+          value={filters.month}
+          onChange={onFilterChange}
+          className="search-input"
+        />
+        <button type="submit" className="search-btn">
+          Apply Filters
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default Filter;
--- src/Components/SightingsList.js
+++ src/Components/SightingsList.js
@@ -1,31 +1,80 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { BACKEND_URL } from "../constants";
 import Navbar from "./Navbar";
+import Filter from "./Filter";
 
 function SightingsList() {
   const [sightings, setSightings] = useState([]);
+  const [filters, setFilters] = useState({
+    year: "",
+    season: "",
+    month: "",
+  });
+
+  const navigate = useNavigate();
+
+  const fetchSightings = async () => {
+    const urlParams = new URLSearchParams(filters);
+    const url = `${BACKEND_URL}/sightings?${urlParams.toString()}`;
+
+    try {
+      await axios.get(url).then((res) => {
+        setSightings(res.data);
+      });
+    } catch (error) {
+      console.log("Error fetching sightings: ", error);
+    }
+  };
 
   useEffect(() => {
-    axios.get(`${BACKEND_URL}/sightings`).then((res) => {
-      console.log(res.data);
-      setSightings(res.data);
-    });
-  }, []);
+    fetchSightings();
 
-  const sightingsList = sightings.map((sighting, index) => (
-    <Link to={`/sightings/${index}`} key={index} className="sightings-list">
-      <div>
-        {sighting.YEAR} {sighting.SEASON} {sighting.MONTH}
-      </div>
-    </Link>
-  ));
+    const hasQueryParams = Object.values(filters).some((value) => value !== "");
+
+    if (!hasQueryParams) {
+      navigate("/sightings");
+    }
+  }, [navigate, filters]);
+
+  const handleFilterChange = (e) => {
+    const { name, value } = e.target;
+    setFilters((prevFilters) => ({
+      ...prevFilters,
+      [name]: value,
+    }));
+
+    const urlParams = new URLSearchParams(filters);
+    const queryString = urlParams.toString();
+
+    navigate(`?${queryString}`);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    fetchSightings();
+  };
 
   return (
     <div className="App">
       <Navbar />
-      <div className="sightings-list-ctn">{sightingsList}</div>
+      <Filter
+        filters={filters}
+        onFilterChange={handleFilterChange}
+        onSubmit={handleSubmit}
+      />
+      {sightings.map((sighting, index) => (
+        <li key={index} className="sightings-list-ctn">
+          <div className="sightings-list">
+            <Link to={`/sightings/${index}`} className="sightings-list">
+              {" "}
+              {sighting.YEAR} {sighting.SEASON} {sighting.MONTH}
+            </Link>
+          </div>
+        </li>
+      ))}
     </div>
   );
 }
