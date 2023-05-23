58e5a553bacd800330ce65f9ac71846eb01f4d4b
prettykaur <pkaur.dev@gmail.com>
2023-05-23 19:29:44 +0800

    Update routes

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from "./App";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SightingsList from "./Components/SightingsList";
 import Sighting from "./Components/Sighting";
+import SightingForm from "./Components/SightingForm";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -12,7 +13,9 @@ root.render(
     <Routes>
       <Route path="/" element={<App />} />
       <Route path="/sightings" element={<SightingsList />} />
-      <Route path="/sightings/:sightingIndex" element={<Sighting />} />
+      <Route path="/sightings/:id" element={<Sighting />} />
+      <Route path="/sightings/:id/edit" element={<Sighting />} />
+      <Route path="/new" element={<SightingForm />} />
       <Route path="*" element={"Oops! Something went wrong."} />
       {/* </Route> */}
     </Routes>
