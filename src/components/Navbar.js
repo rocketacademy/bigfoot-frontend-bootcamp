b35e1fa8d57bda7e095ebc6e1c1dbfeac4314dc4
daniellimss <highdawn01@yahoo.com>
2023-05-29 19:32:44 +0800

    sighting -  edit and delete buttons WIP

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,7 +9,7 @@ export const Navbar = () => {
 				<NavLink to="/">Home</NavLink>
 				<NavLink to="/sightings">Sightings</NavLink>
 				<NavLink to="/newsighting">New Sighting</NavLink>
-				<NavLink to="/editsighting">Edit Sighting</NavLink>
+				{/* <NavLink to="/editsighting">Edit Sighting</NavLink> */}
 			</nav>
 		</>
 
--- src/components/Sightings.js
+++ src/components/Sightings.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { useState, useEffect } from "react";
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 export const Sightings = () => {
 	const [sighting, setSighting] = useState([]);
 	// const [sightingIndex, setSightingIndex] = useState();
+	const navigate = useNavigate()
 
 	useEffect(() => {
 		const getSightings = async () => {
@@ -16,18 +18,31 @@ export const Sightings = () => {
 		getSightings()
 
 	}, []);
+
+	//The following are for tests only:
+	// const handleEdit = () => {
+	// 	console.log('this post is selected for editing')
+	// }
+	// const handleDelete = () => {
+	// 	console.log('this post is selected for deleting')
+	// }
+	// route for deletesighting not yet created
+
+
 	return (
 		<div>
 			{
 				sighting.map((sight) => {
-					return (sighting.length !== 0 ? <div>
+					return (sighting.length !== 0 ?
 						<div>
-							<button>Edit</button>
-							<button>Delete</button>
-						</div>
+							<div>
+								<button onClick={() => navigate('/editsighting')}>Edit</button>
+								<button onClick={() => navigate('/deletesighting')}>Delete</button>
+							</div>
 
-						<p>{sight.date}</p> <p>{sight.location}</p><p>{sight.notes}</p><br />
-						<hr /></div> : null)
+							<p>{sight.date}</p> <p>{sight.location}</p><p>{sight.notes}</p><br />
+							<hr />
+						</div> : null)
 				})
 			}
 
