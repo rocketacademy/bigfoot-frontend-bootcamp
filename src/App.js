1f5eb172b7aaaefe8a3b15fd81a628abfa5a62d1
ryanlerler <lyp2726@outlook.com>
2023-06-24 15:47:36 +0800

    Retrieve sighting data in frontend from backend

--- src/App.js
+++ src/App.js
@@ -1,20 +1,49 @@
 import React from "react";
 import logo from "./logo.png";
 import "./App.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-        </header>
-      </div>
-    );
-  }
+function App() {
+  const [year, setYear] = useState([]);
+  const [season, setSeason] = useState([]);
+  const [month, setMonth] = useState([]);
+  const [country, setCountry] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data } = await axios.get("http://localhost:3000/sightings");
+      console.log(data);
+
+      data.forEach((data) => {
+        setYear((y) => [...y, data.YEAR]);
+        setSeason((s) => [...s, data.SEASON]);
+        setMonth((m) => [...m, data.MONTH]);
+        setCountry((c) => [...c, data.COUNTY]);
+      });
+    };
+
+    fetchData();
+  }, []);
+
+  const dataRendered = year.map((year, index) => (
+    <ul key={year + season + month + country + index}>
+      <li>Sighting {index + 1}</li>
+      <li>Year: {year}</li>
+      <li>Season: {season[index]}</li>
+      <li>Month: {month[index]}</li>
+      <li>Country: {country[index]}</li>
+    </ul>
+  ));
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        {dataRendered}
+      </header>
+    </div>
+  );
 }
 
 export default App;
