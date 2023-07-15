2399471a8ec79d8152bd5e6098558f335a9a276f
Huang Yu Hao <yuhao.huang@ntu.edu.sg>
2023-07-15 12:04:12 +0800

    set up basic display page. TODO: add react routing

--- src/App.js
+++ src/App.js
@@ -1,16 +1,49 @@
-import React from "react";
+import React, { Component } from "react";
 import logo from "./logo.png";
 import "./App.css";
 
-class App extends React.Component {
+const INDEX = 2;
+const url = `http://localhost:3000/sightings/${INDEX}`;
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      sightings: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch(url) // Send GET request to '/sightings' endpoint
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({ sightings: data }); // Update the 'sightings' state with the fetched data
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  }
   render() {
+    const sightings = this.state.sightings;
+
+    console.log(sightings);
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+          {sightings ? (
+            <div>
+              <p>
+                Year: {sightings.YEAR}+{sightings.SEASON}
+              </p>
+
+              <p>
+                Location: {sightings.STATE}+{sightings.COUNTY}
+              </p>
+
+              <p>Location Details: {sightings.LOCATION_DETAILS}</p>
+            </div>
+          ) : null}
         </header>
       </div>
     );
