9fdb175de617cd104127cd89f5e0852717032d11
KenHo95 <mingquan.ho.2017@accountancy.smu.edu.sg>
2023-07-16 21:47:05 +0800

    Implemented display of year and state of sighting

--- src/App.js
+++ src/App.js
@@ -1,20 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import logo from "./logo.png";
 import "./App.css";
+import axios from "axios";
+import { Routes, Route, useNavigate, useLocation } from "react-router-dom";
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-        </header>
-      </div>
-    );
-  }
-}
+const App = () => {
+  const [sightings, setSightings] = useState([]);
+
+  const getSightingData = async () => {
+    const data = await axios.get("http://localhost:3000/sightings");
+
+    setSightings(data.data);
+  };
+
+  useEffect(() => {
+    getSightingData();
+    return;
+  }, []);
+
+  const sightingList =
+    // render list of sighting
+    sightings.map((sighting, ind) => {
+      return <li key={ind}>{`${sighting.YEAR}, ${sighting.STATE}`}</li>;
+    });
+
+  return (
+    <div className="App">
+      {console.log(sightings)}
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <br />
+        <ul className="sighting-list">{sightingList}</ul>
+      </header>
+    </div>
+  );
+};
 
 export default App;
