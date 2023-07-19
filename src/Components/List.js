7bfbe3508ad76db3f422a2d5edfcab30f26a1fee
ryanlerler <lyp2726@outlook.com>
2023-07-20 03:39:36 +0800

    Sort sightings

--- src/Components/List.js
+++ src/Components/List.js
@@ -7,26 +7,25 @@ import { useSearchParams } from "react-router-dom";
 export default function List() {
   const [sightings, setSightings] = useState([]);
   const [userInput, setUserInput] = useState("");
-  const [isFiltered, setIsFiltered] = useState(false);
   const [searchParams, setSearchParams] = useSearchParams();
 
   const year = searchParams.get("year");
+  const sort = searchParams.get("sort");
 
   useEffect(() => {
     const fetchData = async () => {
       const { data } = await axios.get(`${BACKEND_URL}/sightings`, {
-        params: { year: year },
+        params: { year: year, sort: sort },
       });
       console.log(data);
       setSightings(data);
     };
 
     fetchData();
-  }, [year]);
+  }, [year, sort]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    setIsFiltered(true);
     setSearchParams({ year: userInput });
   };
 
@@ -43,44 +42,37 @@ export default function List() {
         />
       </form>
 
-      {isFiltered
-        ? sightings
-            .filter((sighting) => sighting.date.includes(userInput))
-            .map((filteredSighting) => (
-              <ul key={filteredSighting.id}>
-                <Link to={`/sightings/${filteredSighting.id}`}>
-                  <li>Sighting {filteredSighting.id}</li>
-                </Link>
-                <li>Date: {filteredSighting.date}</li>
-                <li>Location: {filteredSighting.location}</li>
-                {filteredSighting.categories.length > 0 && (
-                  <li>
-                    Categories: {""}
-                    {filteredSighting.categories
-                      .map((category) => category.name)
-                      .join(", ")}
-                  </li>
-                )}
-              </ul>
-            ))
-        : sightings.map((sighting) => (
-            <ul key={sighting.id}>
-              <Link to={`/sightings/${sighting.id}`}>
-                <li>Sighting {sighting.id}</li>
-              </Link>
-              <li>Date: {sighting.date}</li>
-              <li>Location: {sighting.location}</li>
+      <button
+        onClick={() =>
+          setSearchParams({ year: year === null ? "" : year, sort: "asc" })
+        }
+      >
+        Sort by asc year
+      </button>
+      <button
+        onClick={() =>
+          setSearchParams({ year: year === null ? "" : year, sort: "desc" })
+        }
+      >
+        Sort by dsc year
+      </button>
 
-              {sighting.categories.length > 0 && (
-                <li>
-                  Categories:{" "}
-                  {sighting.categories
-                    .map((category) => category.name)
-                    .join(", ")}
-                </li>
-              )}
-            </ul>
-          ))}
+      {sightings.map((sighting) => (
+        <ul key={sighting.id}>
+          <Link to={`/sightings/${sighting.id}`}>
+            <li>Sighting {sighting.id}</li>
+          </Link>
+          <li>Date: {sighting.date}</li>
+          <li>Location: {sighting.location}</li>
+
+          {sighting.categories.length > 0 && (
+            <li>
+              Categories:{" "}
+              {sighting.categories.map((category) => category.name).join(", ")}
+            </li>
+          )}
+        </ul>
+      ))}
     </div>
   );
 }
