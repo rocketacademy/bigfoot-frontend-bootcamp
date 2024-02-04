5c9c152b96b1bd96e8f1de544bcff481aa1a9e07
qiannnly <qianlinggg@gmail.com>
2024-02-04 17:27:48 +0800

    added an about page

---
+++ src/component/About.js
@@ -0,0 +1,12 @@
+const About = () => {
+  return (
+    <>
+      <p>
+        This is a big foot sighting page where you are allowed to view more
+        descriptions on each sighting.
+      </p>
+    </>
+  );
+};
+
+export default About;
