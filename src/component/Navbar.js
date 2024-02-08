f2f1906452c3715aa9034323a9445c29b74a9e38
qiannnly <qianlinggg@gmail.com>
2024-02-08 13:59:57 +0800

    added navlinks to home and all sightings

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <>
       <div className="mt-28 mx-24 mb-4  p-12">
-        <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 text-center">
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 text-center bg-orange-100 p-9">
           <div>
             <NavLink to="/">Home</NavLink>
           </div>
