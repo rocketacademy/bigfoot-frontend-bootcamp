26fc3e7f36af1ffda8d19fe51ab954c6ce872d1c
qiannnly <qianlinggg@gmail.com>
2024-02-09 10:45:41 +0800

    updated content

--- src/component/IndividualSighting.js
+++ src/component/IndividualSighting.js
@@ -16,25 +16,31 @@ const IndividualSighting = () => {
     }
   }, [sightingId]);
 
-  const sightingInfo = [];
-  const keys = Object.keys(sightings);
-  keys.forEach((key, index) => {
-    sightingInfo.push(`${key}: ${sightings[key]}`);
-  });
-
-  const details = sightingInfo.map((sighting) => {
-    return <p>{sighting}</p>;
-  });
+  // const sightingInfo = [];
+  // const keys = Object.keys(sightings);
+  // keys.forEach((key, index) => {
+  //   sightingInfo.push(`${index}: ${sightings[key]}`);
+  // });
 
   return (
     <>
-      <div className="grid grid-cols-1 underline m-9">
-        <button type="submit" onClick={() => navigate(-1)}>
-          Go back
+      <div className="flex flex-col items-center justify-center">
+        <button
+          type="submit"
+          className="bg-transparent border border-blue-500 py-2 px-4 hover:bg-blue-500 text-blue-700 font-semibold hover:text-white rounded mb-6"
+          onClick={() => navigate(-1)}
+        >
+          Back to homepage
         </button>
       </div>
 
-      <div className="grid grid-cols-1 text-center">{details}</div>
+      <div className="grid grid-cols-1 text-center text-justify px-48 space-between flex flex-col space-y-4">
+        <p>{`Report number: ${sightings.REPORT_NUMBER}`}</p>
+        <p>{`Season: ${sightings.SEASON}`}</p>
+        <p>{`State: ${sightings.STATE},  ${sightings.COUNTY}`}</p>
+        <p>{`Conditions: ${sightings.TIME_AND_CONDITIONS}`}</p>
+        <p>{`Observed: ${sightings.OBSERVED}`}</p>
+      </div>
     </>
   );
 };
