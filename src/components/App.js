534c9764b8a001a118a2e107605d35dfefc90348
charlesleezhaoyi <charlesleezhaoyi@gmail.com>
2024-02-27 21:33:29 +0800

    updated codebase to reflect latest requirements for Bigfoot SQL 1-m

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,76 @@
 import React, { useState, useEffect } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Link,
+  useParams,
+} from "react-router-dom";
 import logo from "../logo.png";
 import "./App.css";
 import axios from "axios";
+import { Form } from "react-bootstrap";
+
+const Sighting = () => {
+  let { sightingIndex } = useParams();
+  const [sighting, setSighting] = useState({});
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchSighting = async () => {
+      try {
+        const res = await axios.get(
+          `http://localhost:3000/sightings/${sightingIndex}/`
+        );
+        setSighting(res.data);
+        console.log(res.data);
+        setLoading(false);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchSighting();
+  }, [sightingIndex]);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      {sighting && (
+        <div>
+          <p>
+            Location: {sighting.location} <br></br>
+          </p>
+          <p>
+            Note: {sighting.notes} <br></br>
+          </p>
+        </div>
+      )}
+      {/* <Form>
+        <Form.Control type="text" name="location">
+          Hello
+        </Form.Control>
+      </Form> */}
+      {/* {sighting
+          .filter((item) => item.sighting_id === parseInt(sightingIndex))
+          .map((item, index) => (
+            <p key={index}>Comment: {item.content}</p>
+          ))} */}
+      <Link to="/">Back</Link>
+    </div>
+  );
+};
 
 const App = () => {
   const [sightings, setSightings] = useState([]);
-  const [indexedSightings, setIndexSightings] = useState([]);
 
   const displaySightings = async () => {
     try {
-      const res = await axios.get("http://localhost:3000/sightings");
+      const res = await axios.get("http://localhost:3000/sightings/");
+      console.log(res.data);
       setSightings(res.data);
     } catch (error) {
       console.error(error);
@@ -20,33 +81,30 @@ const App = () => {
     displaySightings();
   }, []);
 
-  const displaySightingsIndex = async () => {
-    try {
-      const res = await axios.get("http://localhost:3000/sightings/3");
-      setIndexSightings(res.data);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  useEffect(() => {
-    displaySightingsIndex();
-  });
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p key={indexedSightings.REPORT_NUMBER}>
-          {indexedSightings.REPORT_NUMBER}
-        </p>
-        <div>
-          {sightings.map((sighting) => (
-            <p key={sighting.index}>{sighting.REPORT_NUMBER}</p>
-          ))}
-        </div>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <div>
+                  {sightings.map((sighting, index) => (
+                    <Link key={index} to={`/sightings/${index}`}>
+                      {sighting.id}: {sighting.createdAt}
+                      <br></br>
+                    </Link>
+                  ))}
+                </div>
+              }
+            />
+            <Route path="/sightings/:sightingIndex" element={<Sighting />} />
+          </Routes>
+        </header>
+      </div>
+    </Router>
   );
 };
 
